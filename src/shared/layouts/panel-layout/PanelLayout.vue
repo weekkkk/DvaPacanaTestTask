3b0d51cabbac204ce97e5719b375158a4d09ede3
Nikita Nedelko <nikita.nedelko.job@gmail.com>
2023-07-18 04:00:06 +0300

    update panel layout

--- src/shared/layouts/panel-layout/PanelLayout.vue
+++ src/shared/layouts/panel-layout/PanelLayout.vue
@@ -1,5 +1,22 @@
 <script lang="ts" setup></script>
 
-<template></template>
+<template>
+  <div class="panel_layout f fd-col">
+    <section class="panel_layout-section-top">
+      <slot name="top" />
+    </section>
+    <section class="panel_layout-section-bottom">
+      <slot name="bottom" />
+    </section>
+  </div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.panel_layout {
+  &-section {
+    &-bottom {
+      flex-grow: 1;
+    }
+  }
+}
+</style>
